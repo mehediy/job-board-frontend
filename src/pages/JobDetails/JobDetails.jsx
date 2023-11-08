c9fb85a827e75b62ca946c4e3fafe96428f0e4bf
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-11-08 22:10:11 +0600

    handle job details error

--- src/pages/JobDetails/JobDetails.jsx
+++ src/pages/JobDetails/JobDetails.jsx
@@ -36,6 +36,7 @@ const JobDetails = () => {
   } = useApplyJob();
 
   const { data: job, isPending, isError, error, refetch } = getJob(id);
+  console.log(error);
 
   const [resume, setResume] = useState("");
   const jobApplyHandler = async () => {
@@ -87,92 +88,108 @@ const JobDetails = () => {
         </div>
       ) : (
         <>
-          <Helmet>
-            <title>{job?.data?.title} | Jobs</title>
-          </Helmet>
-          {/* Banner */}
-          <div className="h-[200px] md:h-[400px] xl:h-[450px] px-0 md:px-4">
-            <img
-              className="w-full h-full object-cover"
-              src={job?.data?.banner}
-            />
-          </div>
+          {isError ? (
+            <span className="px-6 py-4 h-[100px] text-center text-error">
+              Error: {error.message}
+            </span>
+          ) : (
+            <>
+              <Helmet>
+                <title>{job?.data?.title || ""} | Jobs</title>
+              </Helmet>
+              {/* Banner */}
+              <div className="h-[200px] md:h-[400px] xl:h-[450px] px-0 md:px-4">
+                <img
+                  className="w-full h-full object-cover"
+                  src={job?.data?.banner}
+                />
+              </div>
 
-          {/* Details */}
-          <div className="padding space-y-4 max-w-5xl mx-auto">
-            {/* Heading */}
-            <div className="flex gap-4 flex-col md:flex-row justify-between items-center md:items-start flex-1">
-              <div className="flex gap-4 flex-col md:flex-row items-center md:items-start">
-                <div className="bg-slate-200 w-[64px] h-[64px]">
-                  {user?.photoURL && <img src={user?.photoURL} />}
+              {/* Details */}
+              <div className="padding space-y-4 max-w-5xl mx-auto">
+                {/* Heading */}
+                <div className="flex gap-4 flex-col md:flex-row justify-between items-center md:items-start flex-1">
+                  <div className="flex gap-4 flex-col md:flex-row items-center md:items-start">
+                    <div className="bg-slate-200 w-[64px] h-[64px]">
+                      {user?.photoURL && <img src={user?.photoURL} />}
+                    </div>
+                    <div className="flex flex-col items-center md:items-start">
+                      <p className="text-brand-primary font-semibold">
+                        {job?.data?.user}
+                      </p>
+                      <h1 className="heading-2">{job?.data?.title}</h1>
+                      <p className="text-light flex items-center gap-2 mt-4">
+                        <img src={peopleIcon} />
+                        Applicants: {job?.data?.applicants}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-center md:items-end gap-4">
+                    <Button
+                      variant={"accent"}
+                      label={"Apply Now"}
+                      // onClick={jobApplyHandler}
+                      onClick={onOpen}
+                    />
+
+                    <p className="text-lg font-semibold">
+                      Salary: {job?.data?.salary}
+                    </p>
+                  </div>
                 </div>
-                <div className="flex flex-col items-center md:items-start">
-                  <p className="text-brand-primary font-semibold">
-                    {job?.data?.user}
-                  </p>
-                  <h1 className="heading-2">{job?.data?.title}</h1>
-                  <p className="text-light flex items-center gap-2 mt-4">
-                    <img src={peopleIcon} />
-                    Applicants: {job?.data?.applicants}
-                  </p>
+                <div className="border-b-2 border-dark mt-4 mb-2"></div>
+                {/* End Heading */}
+                {/* Description */}
+                <div>
+                  <h2 className="heading-2 pb-2">Description</h2>
+                  <p className="text-light">{job?.data?.description}</p>
                 </div>
               </div>
-              <div className="flex flex-col items-center md:items-end gap-4">
-                <Button
-                  variant={"accent"}
-                  label={"Apply Now"}
-                  // onClick={jobApplyHandler}
-                  onClick={onOpen}
-                />
 
-                <p className="text-lg font-semibold">
-                  Salary: {job?.data?.salary}
-                </p>
-              </div>
-            </div>
-            <div className="border-b-2 border-dark mt-4 mb-2"></div>
-            {/* End Heading */}
-            {/* Description */}
-            <div>
-              <h2 className="heading-2 pb-2">Description</h2>
-              <p className="text-light">{job?.data?.description}</p>
-            </div>
-          </div>
+              <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Apply Job</ModalHeader>
+                  <ModalCloseButton />
+                  <ModalBody>
+                    <Input
+                      label={"User Name"}
+                      type={"text"}
+                      name={"user"}
+                      defaultValue={job?.data?.user}
+                      disabled={true}
+                    />
+                    <Input
+                      onChange={(e) => setResume(e.target.value)}
+                      label={"Resume Link"}
+                      type={"text"}
+                      placeholder={"Resume link"}
+                      name={"resume"}
+                    />
+                  </ModalBody>
 
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent>
-              <ModalHeader>Apply Job</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                <Input
-                  label={"User Name"}
-                  type={"text"}
-                  name={"user"}
-                  defaultValue={job?.data?.user}
-                  disabled={true}
-                />
-                <Input
-                  onChange={(e) => setResume(e.target.value)}
-                  label={"Resume Link"}
-                  type={"text"}
-                  placeholder={"Resume link"}
-                  name={"resume"}
-                />
-              </ModalBody>
-
-              <ModalFooter className="space-x-2">
-                <Button
-                  variant={applyingJob || isSuccess ? "disabled" : "accent"}
-                  label={
-                    applyingJob ? "Applying" : isSuccess ? "Applied" : "Apply"
-                  }
-                  onClick={jobApplyHandler}
-                />
-                <Button variant={"outline"} label={"Close"} onClick={onClose} />
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
+                  <ModalFooter className="space-x-2">
+                    <Button
+                      variant={applyingJob || isSuccess ? "disabled" : "accent"}
+                      label={
+                        applyingJob
+                          ? "Applying"
+                          : isSuccess
+                          ? "Applied"
+                          : "Apply"
+                      }
+                      onClick={jobApplyHandler}
+                    />
+                    <Button
+                      variant={"outline"}
+                      label={"Close"}
+                      onClick={onClose}
+                    />
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+            </>
+          )}
         </>
       )}
     </div>
